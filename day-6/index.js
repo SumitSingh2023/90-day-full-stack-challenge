6eaf965d08d3bba7fdd842384b70b6c0429f2c56
Sumit singh <sumit700490@gmail.com>
2024-10-24 16:45:24 +0530

    ok

--- day-6/index.js
+++ day-6/index.js
@@ -1,15 +1,27 @@
-function sayHello(name, age) {
-     this.name = name;
-     this.age = age
+// function sayHello(name, age) {
+//      this.name = name;
+//      this.age = age
 
-    console.log('hello', this)
+//     console.log('hello', this)
+// }
+
+// let person = {
+//     name: "",
+//     age: 0
+// }
+
+// let bindFun = sayHello.bind(person, 'sumit', 24)
+// bindFun()
+
+
+function greet(){
+    console.log('this inside greet',this)
+
+    let arr =()=>{
+        console.log('inside arr',this)
+    }
+
+    arr()
 }
 
-var person = {
-    name: "",
-    age: 0
-}
-
-let bindFun = sayHello.bind(person, 'sumit', 24)
-bindFun()
-
+greet()
